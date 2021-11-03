ab01293b27a4846e6eb8867bfc3246911e84c688
Nick Ang <nicholas_ang@outlook.com>
2021-11-03 18:04:21 +1100

    up to heroku

--- src/Api.js
+++ src/Api.js
@@ -22,29 +22,31 @@ class Api {
   }
 
   async getAll() {
-    return await fetch('http://blogspringbootmysqlkotlin-env.eba-vpdedufk.ap-southeast-2.elasticbeanstalk.com/api/articles/', {
+    return await fetch('http://blogspringbootmysqlkotlin-env.eba-vpdedufk.ap-southeast-2.elasticbeanstalk.com/api/articles', {
       method: "GET",
-      mode: "no-cors",
       headers: this.createHeaders(),
     });
   }
 
   async getById(id) {
-    return await fetch(`${this.BASE_URL}/${id}`, {
-      method: "GET",
-      headers: this.createHeaders(),
-    });
+    return await fetch(
+      `http://blogspringbootmysqlkotlin-env.eba-vpdedufk.ap-southeast-2.elasticbeanstalk.com/api/articles/${id}`,
+      {
+        method: "GET",
+        headers: this.createHeaders(),
+      }
+    );
   }
 
   async delete(id) {
-    return await fetch(`${this.BASE_URL}/${id}`, {
+    return await fetch(`http://blogspringbootmysqlkotlin-env.eba-vpdedufk.ap-southeast-2.elasticbeanstalk.com/api/articles/${id}`, {
       method: "DELETE",
       headers: this.createHeaders(),
     });
   }
 
   async update(item) {
-    return await fetch(`${this.BASE_URL}/${item.id}`, {
+    return await fetch(`http://blogspringbootmysqlkotlin-env.eba-vpdedufk.ap-southeast-2.elasticbeanstalk.com/api/articles/${item.id}`, {
       method: "PUT",
       headers: this.createHeaders(),
       body: JSON.stringify(item),
@@ -52,7 +54,7 @@ class Api {
   }
 
   async create(item) {
-    return await fetch(this.BASE_URL, {
+    return await fetch('http://blogspringbootmysqlkotlin-env.eba-vpdedufk.ap-southeast-2.elasticbeanstalk.com/api/articles', {
       method: "POST",
       headers: this.createHeaders(),
       body: JSON.stringify(item),
