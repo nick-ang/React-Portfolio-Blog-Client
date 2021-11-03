70998487828eae6dde9971783b5c472c7a06f9b5
Nick Ang <nicholas_ang@outlook.com>
2021-11-03 23:52:29 +1100

    styling

--- src/components/About.js
+++ src/components/About.js
@@ -36,7 +36,7 @@ export default function About() {
             </a>
           </div>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-2/6">
+        <div className="lg:max-w-lg lg:w-full md:w-1/6 w-1/6">
           <Lottie loop animationData={lottieJson} play />
         </div>
       </div>
