e2fc02b67f33dfd39e73acea24e2a0b2b685c740
Nick Ang <nicholas_ang@outlook.com>
2021-11-04 10:26:57 +1100

    :update:

--- src/ArticleEdit.js
+++ src/ArticleEdit.js
@@ -90,16 +90,6 @@ class ArticleEdit extends Component {
                 {{
                   height: 150,
                   menubar: false,
-                  plugins: [
-                    "advlist autolink lists link image",
-                    "charmap print preview anchor help",
-                    "searchreplace visualblocks code",
-                    "insertdatetime media table paste wordcount",
-                  ],
-                  toolbar:
-                    "undo redo | formatselect | bold italic | \
-            alignleft aligncenter alignright | \
-            bullist numlist outdent indent | help",
                 }}
                 onEditorChange={this.handleChange}
                 />
