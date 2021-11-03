b7e675eaefcb94ad2452de62d4bfd6da528b8d90
Nick Ang <nicholas_ang@outlook.com>
2021-11-03 16:14:20 +1100

    aws

--- src/Api.js
+++ src/Api.js
@@ -1,57 +1,60 @@
-class Api {
+import { baseUrl } from "./constant";
 
+class Api {
   constructor(authToken) {
     this.authToken = authToken;
   }
 
   headers = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
+    Accept: "application/json",
+    "Content-Type": "application/json",
   };
 
-  BASE_URL = '/api/articles';
+  BASE_URL = "/api/articles";
 
   createHeaders() {
-    return this.authToken ? {
-      ...this.headers,
-      'Authorization': 'Bearer ' + this.authToken
-    } : this.headers;
+    return this.authToken
+      ? {
+          ...this.headers,
+          Authorization: "Bearer " + this.authToken,
+        }
+      : this.headers;
   }
 
   async getAll() {
     return await fetch(this.BASE_URL, {
-      method: 'GET',
-      headers: this.createHeaders()
+      method: "GET",
+      headers: this.createHeaders(),
     });
   }
 
   async getById(id) {
     return await fetch(`${this.BASE_URL}/${id}`, {
-      method: 'GET',
-      headers: this.createHeaders()
+      method: "GET",
+      headers: this.createHeaders(),
     });
   }
 
   async delete(id) {
     return await fetch(`${this.BASE_URL}/${id}`, {
-      method: 'DELETE',
-      headers: this.createHeaders()
+      method: "DELETE",
+      headers: this.createHeaders(),
     });
   }
 
   async update(item) {
     return await fetch(`${this.BASE_URL}/${item.id}`, {
-      method: 'PUT',
+      method: "PUT",
       headers: this.createHeaders(),
-      body: JSON.stringify(item)
+      body: JSON.stringify(item),
     });
   }
 
   async create(item) {
     return await fetch(this.BASE_URL, {
-      method: 'POST',
+      method: "POST",
       headers: this.createHeaders(),
-      body: JSON.stringify(item)
+      body: JSON.stringify(item),
     });
   }
 }
---
+++ src/components/constant.js
@@ -0,0 +1,2 @@
+export const baseUrl =
+  "http://blogspringbootmysqlkotlin-env.eba-vpdedufk.ap-southeast-2.elasticbeanstalk.com";
