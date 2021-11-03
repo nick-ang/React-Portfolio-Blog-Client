fbb7907e1351164fdbfef549a41b16045eebb0b9
Nick Ang <nicholas_ang@outlook.com>
2021-11-03 12:17:03 +1100

    resp

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,8 @@ import lottieJson from "../animations/work.json";
 export default function About() {
   return (
     <section id="about">
-      <div className="container flex items-center">
-        <div className="flex flex-col  text-center">
+      <div className="container mx-auto flex flex-wrap px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Nick.
             <br className="hidden lg:inline-block" />I love to build amazing
@@ -32,12 +32,11 @@ export default function About() {
             </a>
           </div>
         </div>
-        <div className="flex-initial">
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <Lottie
             loop
             animationData={lottieJson}
             play
-
           />
         </div>
       </div>
