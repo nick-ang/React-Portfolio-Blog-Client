f062d4fe253532d8d02fc8a6147813ba5d54eef5
Nick Ang <nicholas_ang@outlook.com>
2021-11-04 15:23:07 +1100

    style

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -20,7 +20,7 @@ export default function Projects() {
         <div className="flex flex-col w-full mb-20">
           <CodeIcon className="mx-auto inline-block w-10 mb-4" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
-            Apps I've Built
+            My Projects
           </h1>
         </div>
         <div className="flex flex-wrap ">
